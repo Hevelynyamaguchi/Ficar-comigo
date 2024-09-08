e831f1453f79cbf40c90ad9cb77fd0035fc9b8c2
Hevelynyamaguchi <160680530+Hevelynyamaguchi@users.noreply.github.com>
2024-09-07 22:06:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('yesButton').addEventListener('click', function() {
+    alert('Sou louca por você!!!!');
+});
+
+document.getElementById('noButton').addEventListener('click', function() {
+    alert('Que pena, mas não vou desistir, porque estou apaixonada por você.');
+});
